28300cc9baa1feb1a8e459dfd29bae1262b64caf
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2020-10-01 18:50:25 -0300

    Updates

---
+++ precache-manifest.fbdd12df99bffe1c0670dc272b792b73.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3f1afd6d86b1da5f3a80c09092b129",
+    "url": "/dev-conquista/index.html"
+  },
+  {
+    "revision": "ba0698ecafb28c7645ad",
+    "url": "/dev-conquista/static/css/main.7b2def08.chunk.css"
+  },
+  {
+    "revision": "419a10a8402a49be54b5",
+    "url": "/dev-conquista/static/js/2.9a94029f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dev-conquista/static/js/2.9a94029f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0698ecafb28c7645ad",
+    "url": "/dev-conquista/static/js/main.0b4d14b4.chunk.js"
+  },
+  {
+    "revision": "180a9501cf2bc81303c2",
+    "url": "/dev-conquista/static/js/runtime-main.191d26b9.js"
+  },
+  {
+    "revision": "fb0efbe0f83962aa57882d633399d27f",
+    "url": "/dev-conquista/static/media/bg.fb0efbe0.png"
+  },
+  {
+    "revision": "13c19830515ad983f49626e4da30a612",
+    "url": "/dev-conquista/static/media/eye.13c19830.svg"
+  },
+  {
+    "revision": "de8555772b93c48ce9325698dd7b2c30",
+    "url": "/dev-conquista/static/media/fork.de855577.svg"
+  },
+  {
+    "revision": "1f2d8a9b88088689354715e142d9baf9",
+    "url": "/dev-conquista/static/media/github.1f2d8a9b.svg"
+  },
+  {
+    "revision": "25b6863a27ff8d6f13ac16067b397053",
+    "url": "/dev-conquista/static/media/javascript.25b6863a.svg"
+  },
+  {
+    "revision": "5e7233cfbc32932a7eb523f9a9d38ad6",
+    "url": "/dev-conquista/static/media/jest.5e7233cf.svg"
+  },
+  {
+    "revision": "611fc7893dec25ed81e177017693fcd4",
+    "url": "/dev-conquista/static/media/link.611fc789.svg"
+  },
+  {
+    "revision": "007eb41ac7dadcaf97d565a787bf8afc",
+    "url": "/dev-conquista/static/media/linkedin.007eb41a.svg"
+  },
+  {
+    "revision": "98cd80765c47b936fe45bfeaf629397b",
+    "url": "/dev-conquista/static/media/message.98cd8076.svg"
+  },
+  {
+    "revision": "de6928a8170976eefbd1eb77ba7db3a5",
+    "url": "/dev-conquista/static/media/phone.de6928a8.svg"
+  },
+  {
+    "revision": "5ab3553ac1af79021a972be8c8883e91",
+    "url": "/dev-conquista/static/media/reactjs.5ab3553a.svg"
+  },
+  {
+    "revision": "382f1e7d2a9c640e8929da4717bbd267",
+    "url": "/dev-conquista/static/media/send.382f1e7d.svg"
+  },
+  {
+    "revision": "23a0f965714828d9c19829feb3083033",
+    "url": "/dev-conquista/static/media/star.23a0f965.svg"
+  },
+  {
+    "revision": "1fa4280237c2f9954d01630a9d47100a",
+    "url": "/dev-conquista/static/media/typescript.1fa42802.svg"
+  }
+]);
